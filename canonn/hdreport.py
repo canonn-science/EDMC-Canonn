1b32c955bffaacbba33aa4d215970bb1e8062cba
NoFoolLikeOne <nofool@paparomeo.uk>
2019-03-03 23:11:11 +0000

    Supress Duplicates
    First execution checks the server to see if the hyperdiction has already been reported we only send new Hyperdictions.  Also using emitter now

--- canonn/hdreport.py
+++ canonn/hdreport.py
@@ -2,36 +2,32 @@ import threading
 import requests
 import sys
 import json
+from emitter import Emitter
 
 '''
-    { 
-        "timestamp":"2018-10-07T13:03:02Z", 
-        "event":"USSDrop", 
-        "USSType":"$USS_Type_NonHuman;", 
-        "USSType_Localised":"Non-Human signal source", 
-        "USSThreat":4 
-    }
+
+We want to record the system where the user was last hyperdicted
+Ths is in the statistics event which appears evert time the user
+returns from the main menu. We want to record   
+
+
 '''
 
-class HDReport(threading.Thread):
+class HDReport(Emitter):
 
     hdsystem=""
 
-    '''
-        Should probably make this a heritable class as this is a repeating pattern
-    '''
-    def __init__(self,cmdr, is_beta, system, station, entry,client):
-        threading.Thread.__init__(self)
+    def __init__(self,cmdr, is_beta, system,entry,client):
+        Emitter.__init__(self,cmdr, is_beta, system, None,None,None, entry, None,None,None,client)
         self.system = system
         self.cmdr = cmdr
-        self.station = station
         self.is_beta = is_beta
         self.entry = entry.copy()
         self.client=client
+        self.modelreport="hdreports"
 
-    def run(self):
+    def setPayload(self):
         payload={}
-
         payload["fromSystemName"]=self.entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM")
         payload["cmdrName"]=self.cmdr  
         payload["isbeta"]= self.is_beta
@@ -40,29 +36,41 @@ class HDReport(threading.Thread):
         payload["reportComment"]="Hyperdiction from TG_ENCOUNTERS"
         payload["hdRawJson"]=self.entry.get("TG_ENCOUNTERS")
 
-        try:        
-            r=requests.post("https://api.canonn.tech:2053/hdreports",data=json.dumps(payload),headers={"content-type":"application/json"})  
-            #print payload
-            #print r
-        except:
-            print("[EDMC-Canonn] Issue posting ussReport " + str(sys.exc_info()[0]))                            
-            print r
-
-def matches(d, field, value):
-	return field in d and value == d[field]	        
-
-'''
-    from canonn import journaldata
-    journaldata.submit(cmdr, system, station, entry)
-'''
-def submit(cmdr, is_beta, system, station, entry,client):
-
-    # The last system isnt always set so we can ignore 
-    if entry["event"] == "Statistics" and entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM"):
+            
+        return payload
         
-        lastsystem=entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM")   
-        if HDReport.hdsystem == lastsystem:
-            print("Hyperdiction already recorded here ")
+    #going to overload run so that we can check for the last hdsystem
+    def run(self):
+        url=self.getUrl()
+        
+        if HDReport.hdsystem == "":
+        
+            r = requests.get("{}/{}?cmdrName={}&_sort=created_at:DESC&_limit=1".format(url,self.modelreport,self.cmdr))
+            j = r.json()
+            if j:
+        
+                HDReport.hdsystem=j[0].get("fromSystemName")
+        
+        if self.entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM") == HDReport.hdsystem:
+            print("Hyperdiction already recorded here - server")
         else:
-            HDReport.hdsystem = lastsystem
-            HDReport(cmdr, is_beta, lastsystem, station, entry,client).start()   
+        
+            payload=self.setPayload()
+            self.send(payload,url)
+
+def submit(cmdr, is_beta, system, station, entry,client):
+    
+    # The last system isnt always set so we can ignore 
+    
+    if entry["event"] == "Statistics" and entry.get("TG_ENCOUNTERS"):
+        # there is no guarentee TG_ENCOUNTER_TOTAL_LAST_SYSTEM will have a value
+        if entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM"):
+        
+            lastsystem=entry.get("TG_ENCOUNTERS").get("TG_ENCOUNTER_TOTAL_LAST_SYSTEM")   
+            if HDReport.hdsystem == lastsystem:
+                print("Hyperdiction already recorded here - session ")
+            else:
+                HDReport(cmdr, is_beta, lastsystem,  entry,client).start()   
+            
+            
+            
