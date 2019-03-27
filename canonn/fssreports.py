56f03804a9fa9115a5db59cfc43f02aa6b8a448a
NoFoolLikeOne <nofool@paparomeo.uk>
2019-03-27 00:29:28 +0000

    Fixing Exclusions
    Also some changes to the AXCZ reporting which I was convinced was working and somehow wasnt,

--- canonn/fssreports.py
+++ canonn/fssreports.py
@@ -55,27 +55,30 @@ class fssEmitter(Emitter):
         #payload["signalName"]=self.entry.get("signalName")
         #payload["signalNameLocalised"]=self.entry.get("signalNameLocalised")
         payload["rawJson"]=self.entry
+        
+        return payload                   
                 
     def getExcluded(self):
         if not fssEmitter.excludefss:
             
-            r=requests.get("{}/excludefssess?_limit=1000".format(self.getUrl()))  
+            r=requests.get("{}/excludefsses?_limit=1000".format(self.getUrl()))  
             if r.status_code == requests.codes.ok:
                 for exc in r.json():
-                    fssEmitter.excludefss["${}_name;".format(exc["signalName"])]=True
+                    fssEmitter.excludefss[exc.get("fssName")]=True
                     
     def run(self):
         
         self.getExcluded()
         
-        #is this a code entry and do we want to record it?
-        if self.entry["event"] == "FSSSignalDiscovered" and not fssEmitter.excludefss.get(self.entry.get("SignalName")) and not self.entry.get("SignalName") == "$USS;" and not self.entry.get("IsStation"):
+        # is this a code entry and do we want to record it?
+        # We dont want o record any that don't begin with $ and and with ;
+        if self.entry["event"] == "FSSSignalDiscovered" and not fssEmitter.excludefss.get(self.entry.get("SignalName")) and not self.entry.get("SignalName") == "$USS;" and not self.entry.get("IsStation") and '$' in self.entry.get("SignalName"):
             
             url=self.getUrl()
             
             if self.entry.get("SignalName") == "$Warzone_TG;":
                 payload=self.getAXPayload()
-                self.modelreport-"axczfssreports"
+                self.modelreport="axczfssreports"
             elif self.entry.get("SignalName") == "$Fixed_Event_Life_Cloud;":
                 debug("Life Cloud")
                 payload=self.getLcPayload()
