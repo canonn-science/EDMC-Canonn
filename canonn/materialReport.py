b486e4015f9326cebbba2f6aac81855949e854d5
VAKazakov <47143965+VAKazakov@users.noreply.github.com>
2019-06-09 19:24:23 +0300

    VAKazakov  Exigius material report (#53)
    * Update load.py
    
    * Update load.py
    
    * Create materialReport.py
    
    * Update materialReport.py
    
    * Update materialReport.py
    
    * hundled changes https://github.com/canonn-science/CAPIv2-Strapi/commit/d29036e5bc26fd24c7647d96c5ec89b443dc57e6
    
    https://github.com/canonn-science/CAPIv2-Strapi/commit/d29036e5bc26fd24c7647d96c5ec89b443dc57e6
    
    * hundled changes https://github.com/canonn-science/CAPIv2-Strapi/commit/d29036e5bc26fd24c7647d96c5ec89b443dc57e6
    
    * Update materialReport.py
    
    * final update of Material reporter
    
    * final patch, all systems operational

---
+++ canonn/materialReport.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import threading
+import requests
+from urllib import quote_plus
+import sys
+import json
+from emitter import Emitter
+from debug import Debug
+from debug import debug,error
+
+
+              
+
+class MaterialsCollected(Emitter):
+    
+    def __init__(self,cmdr, is_beta, system, station, entry,client,lat,lon,body,state,x,y,z,DistFromStarLS):
+        self.state=state
+        self.DistFromStarLS=DistFromStarLS
+        debug("MAterial rep star dist "+str(self.DistFromStarLS))
+        debug("MAterial rep FacState "+str(self.state))
+        Emitter.__init__(self,cmdr, is_beta, system, x,y,z, entry, body, lat, lon,client)       
+
+        self.modelreport="materialreports"
+        
+    def setPayload(self):
+        payload={}
+        payload["system"]=self.system
+        if self.body!=None:
+            payload["body"]=  self.body
+            payload["latitude"]=  self.lat
+            payload["longitude"]=  self.lon
+        else:
+            payload["body"]=  None
+            payload["latitude"]=  self.lat
+            payload["longitude"]=  self.lon
+
+        payload["category"]=self.entry["Category"]
+        payload["journalName"]=self.entry["Name"]        
+        payload["count"]=self.entry["Count"]
+        payload["distanceFromMainStar"] = self.DistFromStarLS
+        payload["coordX"] = self.x
+        payload["coordY"] = self.y
+        payload["coordZ"] = self.z
+        payload["isbeta"]= self.is_beta
+        payload["clientVersion"]= self.client
+        payload["factionState"]=self.state
+
+        return payload
+
+
+def matches(d, field, value):
+    return field in d and value == d[field]    
+    
+
+
+def submit(cmdr, is_beta, system,SysFactionState,DistFromStarLS, station, entry, x,y,z,body,lat,lon,client):
+    if entry["event"] == "MaterialCollected" :
+        MaterialsCollected(cmdr, is_beta, system, station, entry,client,lat,lon,body,SysFactionState,x,y,z,DistFromStarLS).start()   
+        
