6e1aa4cf6855fdb6b1fcd2901b4de687d6119dbe
VAKazakov <47143965+VAKazakov@users.noreply.github.com>
2019-07-02 09:09:32 +0300

    111 (#54)

--- canonn/materialReport.py
+++ canonn/materialReport.py
@@ -48,6 +48,43 @@ class MaterialsCollected(Emitter):
         return payload
 
 
+class MaterialsReward(Emitter):
+    
+    def __init__(self,cmdr, is_beta, system, station, entry,client,lat,lon,body,state,x,y,z,DistFromStarLS):
+        self.state=state
+        self.DistFromStarLS=DistFromStarLS
+        debug("MAterial rep star dist "+str(self.DistFromStarLS))
+        debug("MAterial rep FacState "+str(self.state))
+        Emitter.__init__(self,cmdr, is_beta, system, x,y,z, entry, body, lat, lon,client)       
+
+        self.modelreport="materialreports"
+        
+    def setPayload(self):
+        payload={}
+        payload["system"]=self.system
+        if self.body!=None:
+            payload["body"]=  self.body
+            payload["latitude"]=  self.lat
+            payload["longitude"]=  self.lon
+        else:
+            payload["body"]=  None
+            payload["latitude"]=  self.lat
+            payload["longitude"]=  self.lon
+
+        payload["category"]=self.entry["MaterialsReward"][0]["Category"]
+        payload["journalName"]=self.entry["MaterialsReward"][0]["Name"]
+        #payload["journalLocalised"]=unicode(self.entry["MaterialsReward"][0].get("Name_Localised"))
+        payload["count"]=self.entry["MaterialsReward"][0]["Count"]
+        payload["distanceFromMainStar"] = self.DistFromStarLS
+        payload["coordX"] = self.x
+        payload["coordY"] = self.y
+        payload["coordZ"] = self.z
+        payload["isbeta"]= self.is_beta
+        payload["clientVersion"]= self.client
+        payload["factionState"]=self.state
+        debug(payload)
+        return payload
+
 def matches(d, field, value):
     return field in d and value == d[field]    
     
@@ -55,5 +92,9 @@ def matches(d, field, value):
 
 def submit(cmdr, is_beta, system,SysFactionState,DistFromStarLS, station, entry, x,y,z,body,lat,lon,client):
     if entry["event"] == "MaterialCollected" :
-        MaterialsCollected(cmdr, is_beta, system, station, entry,client,lat,lon,body,SysFactionState,x,y,z,DistFromStarLS).start()   
+        debug(entry)
+        MaterialsCollected(cmdr, is_beta, system, station, entry,client,lat,lon,body,SysFactionState,x,y,z,DistFromStarLS).start()      
+    if  "MaterialsReward" in entry:
+        debug(entry)
+        MaterialsReward  (cmdr, is_beta, system, station, entry,client,lat,lon,body,SysFactionState,x,y,z,DistFromStarLS).start()
         
