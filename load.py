252157cf6073cb8aefd3c007b1cdd725739f5011
NoFoolLikeOne <nofool@paparomeo.uk>
2019-08-05 23:31:32 +0100

    Update Version Number

--- load.py
+++ load.py
@@ -39,7 +39,7 @@ myPlugin = "EDMC-Canonn"
 
 this.SysFactionState = None  # variable for state of controling faction
 this.DistFromStarLS = None  # take distance to star
-this.version = "2.3.1"
+this.version = "2.3.2"
 this.client_version = "{}.{}".format(myPlugin, this.version)
 this.body_name = None
 
