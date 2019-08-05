f1659c19a6d85c0bfb1000cb15722b44df657ef4
Derrick Mehaffy <derrickmehaffy@gmail.com>
2019-08-05 04:55:14 -0700

    Fixing eventAltName and code cleanup

--- canonn/journaldata.py
+++ canonn/journaldata.py
@@ -5,79 +5,73 @@ import json
 import time
 from emitter import Emitter
 from debug import Debug
-from debug import debug,error
-
-
-'''
-Added the above events to: https://api.canonn.tech:2053/excludeevents
--DM
-'''
+from debug import debug, error
 
 class CanonnJournal(Emitter):
     '''
-        Should probably make this a heritable class as this is a repeating pattern
+    Should probably make this a heritable class as this is a repeating pattern
     '''
-    
-    exclusions={}
-    
-    def __init__(self,cmdr, is_beta, system, station, entry,client):
-        Emitter.__init__(self,cmdr, is_beta, system, None,None,None, entry, None,None,None,client)
+
+    exclusions = {}
+
+    def __init__(self, cmdr, is_beta, system, station, entry, client):
+        Emitter.__init__(self, cmdr, is_beta, system, None, None, None, entry, None, None, None, client)
         self.system = system
         self.cmdr = cmdr
         self.station = station
         self.is_beta = is_beta
         self.entry = entry.copy()
         self.client = client
-        self.modelreport="reportevents"
+        self.modelreport = "reportevents"
 
-    def setPayload(self):        
-        payload={}
-        payload["systemName"]=self.system
-        payload["cmdrName"]=self.cmdr  
-        payload["rawJson"]=self.entry
-        payload["eventName"]=self.entry["event"]
-        payload["eventAltName"]=self.entry["Name"]
-        payload["clientVersion"]= self.client
-        payload["isBeta"]= self.is_beta
+    def setPayload(self):
+        payload = {}
+        payload["systemName"] = self.system
+        payload["cmdrName"] = self.cmdr
+        payload["rawJson"] = self.entry
+        payload["eventName"] = self.entry["event"]
+        payload["eventAltName"] = self.entry.get("Name")
+        payload["clientVersion"] = self.client
+        payload["isBeta"] = self.is_beta
         return payload
-        
+
     def run(self):
-        url=self.getUrl()
+        url = self.getUrl()
         if not CanonnJournal.exclusions:
             debug("getting journal excludes")
-            tempexcludes={}
-            r=requests.get("{}/excludeevents?_limit=1000".format(url))
-            
+            tempexcludes = {}
+            r = requests.get("{}/excludeevents?_limit=1000".format(url))
+
             if r.status_code == requests.codes.ok:
                 # populate a local variable so other threads dont see incomplete results
                 for exc in r.json():
-                    tempexcludes[exc["eventName"]]=True
-                CanonnJournal.exclusions=tempexcludes
-                debug("Jouurnal excludes got")
+                    tempexcludes[exc["eventName"]] = True
+                CanonnJournal.exclusions = tempexcludes
+                debug("Journal excludes got")
             else:
                 error("{}/excludeevents".format(url))
 
-        payload={}
-        payload["systemName"]=self.system
-        payload["cmdrName"]=self.cmdr  
-        payload["rawJson"]=self.entry
-        payload["eventName"]=self.entry["event"]
-        payload["eventAltName"]=self.entry["Name"]
-        payload["clientVersion"]= self.client
-        payload["isBeta"]= self.is_beta
-        
-        
-        
-        included_event = not CanonnJournal.exclusions.get(self.entry.get("event"))
-        
+        payload = {}
+        payload["systemName"] = self.system
+        payload["cmdrName"] = self.cmdr
+        payload["rawJson"] = self.entry
+        payload["eventName"] = self.entry["event"]
+        payload["eventAltName"] = self.entry.get("Name")
+        payload["clientVersion"] = self.client
+        payload["isBeta"] = self.is_beta
+
+        included_event = not CanonnJournal.exclusions.get(
+            self.entry.get("event"))
+
         if included_event:
-                    
-            payload=self.setPayload()
-            self.send(payload,url)           
+
+            payload = self.setPayload()
+            self.send(payload, url)
 
 '''
     from canonn import journaldata
     journaldata.submit(cmdr, system, station, entry)
 '''
-def submit(cmdr, is_beta, system, station, entry,client):
-    CanonnJournal(cmdr, is_beta, system, station, entry,client).start()   
+
+def submit(cmdr, is_beta, system, station, entry, client):
+    CanonnJournal(cmdr, is_beta, system, station, entry, client).start()
