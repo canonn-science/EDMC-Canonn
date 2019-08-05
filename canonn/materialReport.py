2cfda283f2f562653c99084e8e04fa84e8d33dcf
VAKazakov <47143965+VAKazakov@users.noreply.github.com>
2019-08-05 14:35:13 +0300

    Another update of MR (#60)
    * Added enum param, which takes collection metod [missionReward,collected,scanned]
    
    * Changed Field name

--- canonn/materialReport.py
+++ canonn/materialReport.py
@@ -16,8 +16,8 @@ class MaterialsCollected(Emitter):
     def __init__(self,cmdr, is_beta, system, station, entry,client,lat,lon,body,state,x,y,z,DistFromStarLS):
         self.state=state
         self.DistFromStarLS=DistFromStarLS
-        debug("MAterial rep star dist "+str(self.DistFromStarLS))
-        debug("MAterial rep FacState "+str(self.state))
+        debug("Material rep star dist "+str(self.DistFromStarLS))
+        debug("Material rep FacState "+str(self.state))
         Emitter.__init__(self,cmdr, is_beta, system, x,y,z, entry, body, lat, lon,client)       
 
         self.modelreport="materialreports"
@@ -33,7 +33,10 @@ class MaterialsCollected(Emitter):
             payload["body"]=  None
             payload["latitude"]=  self.lat
             payload["longitude"]=  self.lon
-
+        if self.entry["Category"]=="Encoded":
+            payload["collectedFrom"]="scanned"
+        else: 
+            payload["collectedFrom"]="collected"
         payload["category"]=self.entry["Category"]
         payload["journalName"]=self.entry["Name"]        
         payload["count"]=self.entry["Count"]
@@ -53,8 +56,8 @@ class MaterialsReward(Emitter):
     def __init__(self,cmdr, is_beta, system, station, entry,client,lat,lon,body,state,x,y,z,DistFromStarLS):
         self.state=state
         self.DistFromStarLS=DistFromStarLS
-        debug("MAterial rep star dist "+str(self.DistFromStarLS))
-        debug("MAterial rep FacState "+str(self.state))
+        debug("Material rep star dist "+str(self.DistFromStarLS))
+        debug("Material rep FacState "+str(self.state))
         Emitter.__init__(self,cmdr, is_beta, system, x,y,z, entry, body, lat, lon,client)       
 
         self.modelreport="materialreports"
@@ -70,7 +73,7 @@ class MaterialsReward(Emitter):
             payload["body"]=  None
             payload["latitude"]=  self.lat
             payload["longitude"]=  self.lon
-
+        payload["collectedFrom"]="missionReward"
         payload["category"]=self.entry["MaterialsReward"][0]["Category"]
         payload["journalName"]=self.entry["MaterialsReward"][0]["Name"]
         #payload["journalLocalised"]=unicode(self.entry["MaterialsReward"][0].get("Name_Localised"))
