b09e65eadbd355591e002c60a56932c9695cf5ff
NoFoolLikeOne <nofool@paparomeo.uk>
2019-09-24 21:24:33 +0100

    Recording client details
    Recording what clients are being used so I can build some sexy charts

--- canonn/clientreport.py
+++ canonn/clientreport.py
@@ -1,44 +1,52 @@
-import threading
+import emitter
+import json
 import requests
 import sys
-import json
-from emitter import Emitter
-from urllib import quote_plus
+import threading
 from debug import Debug
-from debug import debug,error
+from debug import debug, error
+from emitter import Emitter
 from release import Release
+from urllib import quote_plus
+
 
 class clientReport(Emitter):
-    
-    done=False    
-        
-    def __init__(self,cmdr, is_beta,client):
-        
-        self.modelreport="clientreports" 
-        Emitter.__init__(self,cmdr, is_beta, None, None,None,None, None, None,None,None,client)
-        
+    done = False
+
+    def __init__(self, cmdr, is_beta, client):
+
+        self.modelreport = "clientreports"
+        Emitter.__init__(self, cmdr, is_beta, None, None, None, None, None, None, None, None, client)
+
     def setPayload(self):
-        payload={}
-        payload["cmdrName"]=self.cmdr  
-        payload["isBeta"]=self.is_beta
-        payload["clientVersion"]=self.client
+        payload = {}
+        payload["cmdrName"] = self.cmdr
+        payload["isBeta"] = self.is_beta
+        payload["clientVersion"] = self.client
         if Release.get_auto() == 1:
-            payload["AutoUpdateDisabled"]=False
+            payload["AutoUpdateDisabled"] = False
         else:
-            payload["AutoUpdateDisabled"]=True
-            
-        return payload  
+            payload["AutoUpdateDisabled"] = True
+
+        return payload
 
     def run(self):
-        if not clientReport.done:           
-            debug("sending client report")    
-            #configure the payload       
-            payload=self.setPayload()
-            url=self.getUrl()
-            self.send(payload,url)        
-            clientReport.done=True
+        if not clientReport.done:
+            clientReport.done = True
+            debug("sending client report")
+            # configure the payload
+            payload = self.setPayload()
+            url = self.getUrl()
+            self.send(payload, url)
+            emitter.post("https://europe-west1-canonn-api-236217.cloudfunctions.net/postClient",
+                         {
+                             "cmdr": payload.get("cmdrName"),
+                             "beta": payload.get("isBeta"),
+                             "client": payload.get("clientVersion"),
+                             "autoupdate": payload.get("AutoUpdateDisabled")
+                         })
 
-def submit(cmdr, is_beta, client,entry):  
-    
-    if entry.get("event") in ("Location","StartUp"):
-        clientReport(cmdr, is_beta, client).start()   
+
+def submit(cmdr, is_beta, client, entry):
+    if entry.get("event") in ("Location", "StartUp"):
+        clientReport(cmdr, is_beta, client).start()
