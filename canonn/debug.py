5fab66781946a0a8b62b6088dcab8bd4eab67cb9
NoFoolLikeOne <nofool@paparomeo.uk>
2020-03-04 00:15:38 +0000

    Flushing Stdout

--- canonn/debug.py
+++ canonn/debug.py
@@ -5,53 +5,54 @@ except:
     import Tkinter as tk
     from Tkinter import Frame
 
+import datetime
 import myNotebook as nb
-
+import sys
 from config import config
-import datetime 
+
 
 class Debug:
-        
-    debugvar=tk.IntVar(value=config.getint("CanonnDebug"))
-    client="Canonn"
-    
+    debugvar = tk.IntVar(value=config.getint("CanonnDebug"))
+    client = "Canonn"
+
     @classmethod
-    def setClient(cls,client):
-        Debug.client=client
-            
+    def setClient(cls, client):
+        Debug.client = client
+
     @classmethod
-    def p(cls,value):
-        print("{} [{}] {}".format(datetime.datetime.now(),Debug.client,str(value)))
-            
+    def p(cls, value):
+        print("{} [{}] {}".format(datetime.datetime.now(), Debug.client, str(value)))
+        sys.stdout.flush()
+
     @classmethod
-    def debug(cls,value):
-        
+    def debug(cls, value):
         if cls.debugvar.get() == 1:
             cls.p(value)
 
-        
     @classmethod
-    def plugin_prefs(cls, parent,client,gridrow):
+    def plugin_prefs(cls, parent, client, gridrow):
         "Called to get a tk Frame for the settings dialog."
 
-        cls.debugvar=tk.IntVar(value=config.getint("CanonnDebug"))
-        Debug.client=client
-        
+        cls.debugvar = tk.IntVar(value=config.getint("CanonnDebug"))
+        Debug.client = client
+
         frame = nb.Frame(parent)
         frame.columnconfigure(1, weight=1)
-        frame.grid(row = 0, column = 0,sticky="NSEW")
-        
-        nb.Checkbutton(frame, text="Turn on Debugging", variable=cls.debugvar).grid(row = 0, column = 0,sticky="NW")
-        
+        frame.grid(row=0, column=0, sticky="NSEW")
+
+        nb.Checkbutton(frame, text="Turn on Debugging", variable=cls.debugvar).grid(row=0, column=0, sticky="NW")
+
         return frame
-        
-    @classmethod    
+
+    @classmethod
     def prefs_changed(cls):
         "Called when the user clicks OK on the settings dialog."
-        config.set('CanonnDebug', cls.debugvar.get())      
-        
+        config.set('CanonnDebug', cls.debugvar.get())
+
+
 def debug(value):
     Debug.debug(value)
-    
+
+
 def error(value):
-    Debug.p(value)    
+    Debug.p(value)
