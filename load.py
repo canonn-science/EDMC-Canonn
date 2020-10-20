57ada876a2978a2df76b77039775ea80f432e861
NoFoolLikeOne <nofool@paparomeo.uk>
2020-10-20 23:52:52 +0100

    Starting New Relase

--- load.py
+++ load.py
@@ -45,7 +45,7 @@ this.SysFactionState = None  # variable for state of controling faction
 this.SysFactionAllegiance = None  # variable for allegiance of controlling faction
 this.DistFromStarLS = None  # take distance to star
 
-this.version = "5.22.0"
+this.version = "5.23.0"
 
 this.client_version = "{}.{}".format(myPlugin, this.version)
 this.body_name = None
