796d1da9c2d8baa99d9f6abcf1813a76b724d8dd
NoFoolLikeOne <nofool@paparomeo.uk>
2021-01-16 23:53:14 +0000

    Removing timed update
    We only need an update once per day

--- canonn/whitelist.py
+++ canonn/whitelist.py
@@ -6,87 +6,88 @@ except:
     from urllib import quote_plus
     from Tkinter import Frame
     import Tkinter as tk
-    
+
 from canonn.debug import Debug
-from canonn.debug import debug,error
+from canonn.debug import debug, error
 import threading
 import requests
 import sys
 import json
 
 
-
-
 class whiteListGetter(threading.Thread):
-    def __init__(self,callback):
+    def __init__(self, callback):
         threading.Thread.__init__(self)
-        self.callback=callback
+        self.callback = callback
 
     def run(self):
         debug("getting whiteList")
-        url="https://us-central1-canonn-api-236217.cloudfunctions.net/whitelist"
-        r=requests.get(url)   
-            
+        url = "https://us-central1-canonn-api-236217.cloudfunctions.net/whitelist"
+        r = requests.get(url)
+
         if not r.status_code == requests.codes.ok:
             error("whiteListGetter {} ".format(url))
             error(r.status_code)
             error(r.json())
-            results=[]
+            results = []
         else:
-           results=r.json()
-           
+            results = r.json()
+
         self.callback(results)
-        
+
+
 class whiteListSetter(threading.Thread):
 
-    def __init__(self,cmdr, is_beta, system, station, entry, state,x,y,z,body,lat,lon,client):
+    def __init__(self, cmdr, is_beta, system, station, entry, state, x, y, z, body, lat, lon, client):
         threading.Thread.__init__(self)
-        self.cmdr=quote_plus(cmdr.encode('utf8'))
-        self.system=quote_plus(system.encode('utf8'))
-        self.is_beta=is_beta
-        self.station=station
-        self.body=body
-        self.entry=entry
-        self.state=state
-        self.x=x
-        self.y=y
-        self.z=z
-        self.lat=lat
-        self.lon=lon
-        self.client=client
-        
+        self.cmdr = quote_plus(cmdr.encode('utf8'))
+        self.system = quote_plus(system.encode('utf8'))
+        self.is_beta = is_beta
+        self.station = station
+        self.body = body
+        self.entry = entry
+        self.state = state
+        self.x = x
+        self.y = y
+        self.z = z
+        self.lat = lat
+        self.lon = lon
+        self.client = client
+
     def run(self):
-        
-        url="https://us-central1-canonn-api-236217.cloudfunctions.net/submitRaw?"
-        url=url+"&cmdrName={}".format(self.cmdr)
-        url=url+"&systemName={}".format(self.system)
-        url=url+"&bodyName={}".format(self.body)
-        url=url+"&station={}".format(self.station)
-        url=url+"&event={}".format(self.entry.get("event"))
-        url=url+"&x={}".format(self.x)
-        url=url+"&y={}".format(self.y)        
-        url=url+"&z={}".format(self.z)        
-        url=url+"&lat={}".format(self.lat)        
-        url=url+"&lon={}".format(self.lon)        
-        url=url+"&is_beta={}".format(self.is_beta)        
-        url=url+"&raw_event={}".format(quote_plus(json.dumps(self.entry, ensure_ascii=False).encode('utf8')))
-        url=url+"&clientVersion={}".format(self.client)
-        
-        r=requests.get(url)
-            
+
+        url = "https://us-central1-canonn-api-236217.cloudfunctions.net/submitRaw?"
+        url = url+"&cmdrName={}".format(self.cmdr)
+        url = url+"&systemName={}".format(self.system)
+        url = url+"&bodyName={}".format(self.body)
+        url = url+"&station={}".format(self.station)
+        url = url+"&event={}".format(self.entry.get("event"))
+        url = url+"&x={}".format(self.x)
+        url = url+"&y={}".format(self.y)
+        url = url+"&z={}".format(self.z)
+        url = url+"&lat={}".format(self.lat)
+        url = url+"&lon={}".format(self.lon)
+        url = url+"&is_beta={}".format(self.is_beta)
+        url = url+"&raw_event={}".format(quote_plus(
+            json.dumps(self.entry, ensure_ascii=False).encode('utf8')))
+        url = url+"&clientVersion={}".format(self.client)
+
+        r = requests.get(url)
+
         if not r.status_code == requests.codes.ok:
             error("whiteListSetter {} ".format(url))
             error(r.status_code)
             error(r.json())
-            results=[]
+            results = []
         else:
-           results=r.json()
-           
-        
-        
+            results = r.json()
+
+
 '''
   Going to declare this aa a frame so I can run a time
-'''        
+'''
+
+
 class whiteList(Frame):
 
     whitelist = []
@@ -96,43 +97,34 @@ class whiteList(Frame):
             self,
             parent
         )
-     
+
     '''
         if all the keys match then return true
-    '''    
+    '''
     @classmethod
-    def matchkeys(cls,event,entry):
-        
-        ev=json.loads(event)
+    def matchkeys(cls, event, entry):
+
+        ev = json.loads(event)
         for key in ev.keys():
             if not entry.get(key) == ev.get(key):
                 return False
-         
+
         return True
-        
-    @classmethod 
-    def journal_entry(cls,cmdr, is_beta, system, station, entry, state,x,y,z,body,lat,lon,client):
-               
+
+    @classmethod
+    def journal_entry(cls, cmdr, is_beta, system, station, entry, state, x, y, z, body, lat, lon, client):
+
         for event in whiteList.whitelist:
-           
-            if cls.matchkeys(event.get("definition"),entry):
+
+            if cls.matchkeys(event.get("definition"), entry):
                 debug("Match {}".format(entry.get("event")))
-                whiteListSetter(cmdr, is_beta, system, station, entry, state,x,y,z,body,lat,lon,client).start()
-            
-    
-    '''
-      We will fetch the whitelist on the hour
-    '''
-    
+                whiteListSetter(cmdr, is_beta, system, station, entry,
+                                state, x, y, z, body, lat, lon, client).start()
+
+    # we will only load this on startup
+
     def fetchData(self):
         whiteListGetter(self.whiteListCallback).start()
-        self.after(1000*60*60,self.fetchData)
-        
- 
-    def whiteListCallback(self,data):
-        whiteList.whitelist=data
-        
-    
 
-        
-        
+    def whiteListCallback(self, data):
+        whiteList.whitelist = data
