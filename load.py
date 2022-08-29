848a3071c5bfee1856d75bc31672375b79c911c1
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2022-08-29 18:08:18 +0100

    New Release coming

--- load.py
+++ load.py
@@ -75,7 +75,7 @@ this.SysFactionState = None  # variable for state of controling faction
 this.SysFactionAllegiance = None  # variable for allegiance of controlling faction
 this.DistFromStarLS = None  # take distance to star
 
-this.version = "6.9.4"
+this.version = "6.9.5"
 
 this.client_version = "{}.{}".format(myPlugin, this.version)
 this.body_name = None
